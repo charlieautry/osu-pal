df478ae50c9e0ec3c16e0a6cb076826c7e774972
Charlie <charlielautry@gmail.com>
2025-11-02 03:54:23 -0600

    Final

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -10,8 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  basePath: '',
-  assetPrefix: '',
 };
 
 export default nextConfig;
